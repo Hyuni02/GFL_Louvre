5b9921709c1cb229d4d55eba10ab7b2cac250df2
Hyuni02 <jackjsh@naver.com>
2024-04-29 17:23:03 +0900

    스킨 상세 보기 구현

--- GLF_Louvre.py
+++ GLF_Louvre.py
@@ -17,10 +17,11 @@ titles = []
 urls = []
 links = []
 fullimgs = []
+
 # 전역 변수로 이미지 참조를 저장할 리스트 추가
-global_images = []
-global_sprites = []
-global_sprites_thumb = []
+global_images = []  # 캐릭터 썸네일
+global_sprites = []  # 스킨 원본
+global_sprites_thumb = []  # 스킨 썸네일
 
 # class='pad'인 span 태그 하위에 있는 a 태그의 title을 리스트에 추가
 for span in spans:
@@ -47,13 +48,20 @@ current_page = 0
 frame = Frame(window)
 frame.pack(expand=True, fill='both')
 
+# 스킨을 표시할 프레임
+frame2 = Frame(window)
+frame2.pack(expand=True, fill='both')
 
+
+# 메인화면
 def display_main():
     update_buttons(current_page)
     window.geometry("768x240")
 
 
-def display_detailList():
+# 스킨 상세보기
+def display_detail_list(_idx=0):
+    loading_start()
     clear_display()
     window.geometry("1024x642")
 
@@ -61,22 +69,28 @@ def display_detailList():
                       command=lambda: [print("Press btn : back"), display_main()])
     btn_main.grid(row=0, column=0, sticky='w', padx=10)
 
-    global global_sprites
     global global_sprites_thumb
     for i in range(len(global_sprites_thumb)):
         if i % 2 == 1:
             continue
         button = Button(frame, image=global_sprites_thumb[i],
-                        command=lambda _idx=i: display_detail(_idx))
+                        command=lambda _i=i: display_detail_list(_i))
         button.grid(row=1, column=int(i / 2), padx=10, pady=10)  # padx와 pady는 버튼 사이의 간격 조정
         # button.grid(row=1, column=i, padx=10, pady=10)  # padx와 pady는 버튼 사이의 간격 조정
+    display_detail(_idx)
+    loading_end()
 
 
+# 선택한 스킨 표시
 def display_detail(_idx):
-    print(fullimgs[_idx])
-    print(fullimgs[_idx + 1])
+    global global_sprites
+    button = Button(frame2, image=global_sprites[_idx], command=lambda: print(fullimgs[_idx]))
+    button.grid(row=0, column=0, padx=10, pady=10)
+    button2 = Button(frame2, image=global_sprites[_idx + 1], command=lambda: print(fullimgs[_idx + 1]))
+    button2.grid(row=0, column=1, padx=10, pady=10)
 
 
+# 로딩 시작
 def loading_start():
     # loading 메시지 표시
     global loading_label
@@ -85,6 +99,7 @@ def loading_start():
     window.update_idletasks()
 
 
+# 로딩 종료
 def loading_end():
     global loading_label
     loading_label.destroy()
@@ -96,6 +111,7 @@ def clear_display():
         widget.destroy()
 
 
+# 선택한 페이지 보기(메인화면)
 def update_buttons(page=0):
     print(f"Update Page : {page}")
     global current_page
@@ -137,10 +153,12 @@ def fetch_image(_url):
     response_img = requests.get(_url)
     image = response_img.content
     pil_image = Image.open(io.BytesIO(image))
-    pil_image2 = pil_image.resize((120, 120))
-    return ImageTk.PhotoImage(pil_image), ImageTk.PhotoImage(pil_image2)
+    pil_image_thumb = pil_image.resize((120, 120))
+    pil_image_skin = pil_image.resize((360, 360))
+    return ImageTk.PhotoImage(pil_image), ImageTk.PhotoImage(pil_image_thumb), ImageTk.PhotoImage(pil_image_skin)
 
 
+# 선택한 캐릭터 스킨 불러오기 및 표시
 def get_detailpage(_url):
     loading_start()
     print('https://iopwiki.com/' + _url)
@@ -179,11 +197,11 @@ def get_detailpage(_url):
             fullimg_url = img_url[:img_url.find('.png') + 4]
             fullimgs.append(fullimg_url)
             _img = fetch_image(fullimg_url)
-            global_sprites.append(_img[0])
+            global_sprites.append(_img[2])
             global_sprites_thumb.append(_img[1])
 
     loading_end()
-    display_detailList()
+    display_detail_list()
 
 
 # 최초의 버튼 업데이트
